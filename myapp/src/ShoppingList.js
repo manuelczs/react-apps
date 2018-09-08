9fd18d3c27d1c6d15237a9abe49afcf98263a262
manuelczs <manuelczs@outlook.com>
2018-09-08 00:01:21 -0300

    shopping-list added

---
+++ myapp/src/ShoppingList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class ShoppingList extends Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default ShoppingList;
